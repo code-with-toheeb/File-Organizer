0b63527b8a763709b1721acb4ec605ecc47983d8
Olanrewaju Toheeb Olawale <toheebolawale019@gmail.com>
2025-07-30 04:32:02 +0100

    Add Error Handler for invalid folder path

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import os
 import shutil
 
 working_dir = input("Enter the full path of the folder you want to organize: ").strip()
+
+if not os.path.exists(working_dir):
+    print("The folder you enter does not exist")
+    exit()
+
 os.chdir(working_dir)
 os.makedirs(os.path.join(working_dir, "files"), exist_ok=True)
 os.makedirs(os.path.join(working_dir, "directories"), exist_ok=True)
